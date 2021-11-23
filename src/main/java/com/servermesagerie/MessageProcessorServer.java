9ca424217f6f8202f735dcb3a78e8f954ceded5f
Armand <sebapatasanu@yahoo.com>
2021-11-23 18:59:22 +0200

    Consumer basic functionality [Code with me session with Andreea Pintea]

--- src/main/java/com/servermesagerie/MessageProcessorServer.java
+++ src/main/java/com/servermesagerie/MessageProcessorServer.java
@@ -1,8 +1,49 @@
 package com.servermesagerie;
 
-import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+import java.util.logging.Logger;
 
 public class MessageProcessorServer {
 
-    private static Producer serverProducer;
+    public static void main(String[] args)
+    {
+        final Logger logger= (Logger) LoggerFactory.getLogger(ChatClient.class);
+        final String bootstrapServers = "127.0.0.1:9092";
+        final String consumerGroupID = "java-group-consumer";
+        Properties p = new Properties();
+        p.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        p.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        p.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        p.setProperty(ConsumerConfig.GROUP_ID_CONFIG, consumerGroupID);
+        p.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest"); //cred ca merge sters asta dupa
+
+        final KafkaConsumer<String, String> consumer =new KafkaConsumer<String, String>(p);
+        consumer.subscribe(Arrays.asList("java-topic"));
+
+        while (true)
+        {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000));
+            for (ConsumerRecord record:records)
+            {
+                logger.info("Received new record: \n" +
+                        "Key: " + record.key() + ", " +
+                        "Value: " + record.value() + ", " +
+                        "Topic: " + record.topic() + ", " +
+                        "Partition: " + record.partition() + ", " +
+                        "Offset: " + record.offset());
+            }
+        }
+
+
+
+    }
 }
