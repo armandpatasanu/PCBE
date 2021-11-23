aef9567660f123f684d493fb7bb1b611c1971af1
Armand <sebapatasanu@yahoo.com>
2021-11-23 18:55:36 +0200

    Producer basic functionality with CLI topic and consumer

--- src/main/java/com/servermesagerie/ChatClient.java
+++ src/main/java/com/servermesagerie/ChatClient.java
@@ -2,7 +2,9 @@ package com.servermesagerie;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -10,6 +12,24 @@ import java.util.Properties;
 
 public class ChatClient {
 
+    public static void main(String[] args)
+    {
+        Properties prop = new Properties();
+        prop.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        prop.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        prop.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        final KafkaProducer<String, String> producer = new KafkaProducer<String, String>(prop);
+
+        ProducerRecord<String, String> record = new ProducerRecord<>("sample-topic", "key1", "value1");
+
+        producer.send(record);
+
+        producer.flush();
+        producer.close();
+    }
+
+    /*
     private static User user;
     private static KafkaProducer<String, String> producer;
 
@@ -62,4 +82,5 @@ public class ChatClient {
     {
 
     }
+    */
 }
