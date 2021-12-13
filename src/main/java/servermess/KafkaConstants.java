0d3633110302eb6dc9ee13dda3438edcf996a384
Andreea Pintea <andreeapinteam@gmail.com>
2021-12-13 13:07:51 +0200

    Added separate ping handler

--- src/main/java/servermess/KafkaConstants.java
+++ src/main/java/servermess/KafkaConstants.java
@@ -8,4 +8,5 @@ public class KafkaConstants {
     public static final String SERVER_CLIENT_TOPIC = "kafka-server-client";
     // Kafka Brokers
     public static final String BOOTSTRAP_SERVERS = "localhost:9092";
+    public static final String PING_TOPIC = "kafka-ping";
 }
